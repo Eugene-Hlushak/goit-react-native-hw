6aa2cee5b37defcd0bff70642636e02855c61502
Eugene Hlushak <hlushak.e@gmail.com>
2023-03-28 00:18:54 +0300

    add firebase config

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHbc4AeiOu1MgfQ7kvJWrLlcPBXUA6MwI",
+  authDomain: "goit-react-native-hw-8525a.firebaseapp.com",
+  projectId: "goit-react-native-hw-8525a",
+  storageBucket: "goit-react-native-hw-8525a.appspot.com",
+  messagingSenderId: "569169432433",
+  appId: "1:569169432433:web:723f202853cef5f3e670d0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
